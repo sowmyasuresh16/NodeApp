06b8abb468b8c80997b1394e5b7e71695359f765
Sowmya Suresh <sowmyasuresh1620@gmail.com>
2019-11-02 15:06:49 -0500

    Final Commit

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 // Statement to just let the user know that app execution has begun
 console.log('Starting app.js');
-
-// Importing file system dependency package and storing in a variable
-const fs = require('fs');
-
-var express = require("express");
-
+// Importing express library
+var express = require('express');
+// Importing handlebars
+var exphbs = require('express-handlebars');
+// Importing departments json
+const json = require('./departments.json');
+// Importing path library to set node server path
+const path = require('path');
+// Importing dotenv package to access environment variables
 require('dotenv').config();
-
+// Initializing express object
 var app = express();
+
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+// Setting the views and letting node know where to look for hbs files
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.use('/', express.static('public/public'));
 
+// Rendering view to the browser
 app.get('/', function (req, res) {
-    res.send('<html><body><h1>Node App</h1></body></html>');
+    res.render('app', {
+        layout: false,
+        title: 'Node.JS',
+        data: json
+    });
 });
+
 // Sets port 8080 to default or unless otherwise specified in the environment
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+    port = 8080;
 }
+
 app.listen(port);
 
 // Importing the department.js file which has all the methods to perform CRUD operation on department collection in mongodb
-const dept= require('./department.js');
+const dept = require('./department.js');
 
 // Importing loadash module
 const _ = require('lodash');
@@ -44,30 +61,30 @@ console.log('Yargs', argv);
 
 // Department functions begins
 
-// 
+// Add a new department
 if (command === 'add') {
-  var department = dept.addDept(argv.dept_no, argv.dept_name);
-  if (dept){
-    console.log('Department created');
-    console.log('--');
-    console.log(`Number: ${department.dept_no}`);
-    console.log(`Name: ${department.dept_name}`);
-  } else {
-    console.log('Department already exists');
-  }
-} 
+    var department = dept.addDept(argv.dept_no, argv.dept_name);
+    if (dept) {
+        console.log('Department created');
+        console.log('--');
+        console.log(`Number: ${department.dept_no}`);
+        console.log(`Name: ${department.dept_name}`);
+    } else {
+        console.log('Department already exists');
+    }
+}
 // List all departments
 else if (command === 'list') {
-  var allDept = dept.getAll();
+    var allDept = dept.getAll();
     console.log(`The total number of departments is ${allDept.length}`);
     allDept.forEach((deptartment) => {
         dept.logDept(deptartment);
     });
-} 
-// Get all departments
+}
+// Get department based on given department no
 else if (command === 'read') {
-   var department = dept.getDept(argv.dept_no);
-    if(dept) {
+    var department = dept.getDept(argv.dept_no);
+    if (dept) {
         console.log("Found Department");
         console.log('-------------');
         console.log(`Department Number: ${department.dept_no}`);
@@ -76,12 +93,12 @@ else if (command === 'read') {
     else {
         console.log("Department not found");
     }
-} 
-// Delete department
+}
+// Delete department based on department no
 else if (command === 'remove') {
     dept.removeDept(argv.dept_no);
-} 
+}
 // If a command other than the above mentioned commnd is given
 else {
-  console.log('Command not recognized');
+    console.log('Command not recognized');
 }
--- department.js
+++ department.js
@@ -1,6 +1,6 @@
 console.log('Starting department.js');
 const fs = require('fs');
-
+// Reading departments from departments.json
 var fetchDepts = () => {
     try {
         var deptString = fs.readFileSync('departments.json');
@@ -10,16 +10,20 @@ var fetchDepts = () => {
     }
 };
 
+// Save departments by writing to json file
 var saveDepts = (dept) => {
     fs.writeFileSync('departments.json', JSON.stringify(dept));
 };
 
+// Add a new department.
 var addDept = (dept_no, dept_name) => {
     var depts = fetchDepts();
     var dept = {
         dept_no,
         dept_name
     };
+
+    // Checking if a department with the same dept no exists and then pushing the newly added dept
     var duplicateDepts = depts.filter((dept) => dept.dept_no === dept_no);
     if (duplicateDepts.length === 0) {
         depts.push(dept);
@@ -28,11 +32,13 @@ var addDept = (dept_no, dept_name) => {
     }
 };
 
+// Fetch all departments
 var getAll = () => {
     console.log('Getting all Departments');
     return fetchDepts();
 };
 
+// Get department based on given department no
 var getDept = (dept_no) => {
     console.log('Getting Department', dept_no);
     var depts = fetchDepts();
@@ -40,15 +46,17 @@ var getDept = (dept_no) => {
     return filteredDepts[0];
 };
 
+// Remove department based on given department no
 var removeDept = (dept_no) => {
     console.log('Removing Department', dept_no);
 };
-
+// Logging Departments info
 var logDept = (dept) => {
     console.log("-------------");
     console.log('Department Number', dept.dept_no);
     console.log('Department Name', dept.dept_name);
 }
+// Exporting methods inorder to use it in app.js
 module.exports = {
     addDept,
     getAll,
